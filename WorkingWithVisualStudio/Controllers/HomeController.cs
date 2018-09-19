97bc3662df7ace54173904cf2c6d0d9c3cbc3cad
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-19 09:25:04 +0300

    Add project files.

---
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Models;
+using System.Linq;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
+        public IActionResult Index()
+        {
+            return View(Repository.Products.Where(p => p?.Price < 50));
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
