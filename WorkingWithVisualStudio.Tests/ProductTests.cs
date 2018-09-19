8a86a48a383d523ef137d23608feda1e54a29383
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-19 10:08:49 +0300

    Running Tests with the Test Explorer, Correcting a Test

--- WorkingWithVisualStudio.Tests/ProductTests.cs
+++ WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -28,7 +28,7 @@ namespace WorkingWithVisualStudio.Tests
             p.Price = 200M;
 
             //Assert
-            Assert.Equal(100M, p.Price);
+            Assert.Equal(200M, p.Price);
         }
     }
 }
